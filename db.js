1b3a0582c7846c7d57e70ba3132c611be7b71fb1
FIL1994 <phil.vanraalte@gmail.com>
2017-09-22 19:45:45 -0400

    initial commit

---
+++ db.js
@@ -0,0 +1,42 @@
+/**
+ * @author Philip Van Raalte
+ * @date 2017-09-22.
+ */
+const keys = require('./keys');
+const Database = require('arangojs').Database;
+const username = keys.username;
+const password = keys.password;
+const db = new Database(`http://${keys.host}:8529`);
+db.useDatabase(keys.dbName);
+
+db.useBasicAuth(username, password);
+
+module.exports = {
+  listCollections: async (nameOnly = true) => {
+    let collections = await db.listCollections();
+
+    if(nameOnly) {
+      collections = collections.map((c) => {
+        return c.name
+      });
+    }
+
+    return collections;
+  },
+  getCollection: async name => {
+    const collection = db.collection(name);
+    return collection;
+  },
+  insert: async (collection, object) => {
+    return await collection.save(object, {returnNew: true});
+  },
+  update: async (collection, documentHandle, object) => {
+
+    //documentHandle -> _id or _key or a document
+    return await collection.update(documentHandle, object);
+  },
+  selectAll: async (collection) => {
+    return await collection.all();
+  }
+
+};
